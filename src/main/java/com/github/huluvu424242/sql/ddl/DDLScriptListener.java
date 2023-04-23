82131099e3b06e53acc5886251a0422efede83cc
huluvu424242 <huluvu424242@gmail.com>
2023-04-23 11:48:47 +0200

    [feature/projectsetup] mit license added
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/main/java/com/github/huluvu424242/sql/ddl/DDLScriptListener.java
+++ src/main/java/com/github/huluvu424242/sql/ddl/DDLScriptListener.java
@@ -1,18 +1,42 @@
 package com.github.huluvu424242.sql.ddl;
 
+/*-
+ * #%L
+ * java-compiler-template
+ * %%
+ * Copyright (C) 2023 PIUG
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
 
 import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLBaseListener;
 import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLParser;
+import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.tree.ErrorNode;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+@Slf4j
 public class DDLScriptListener extends SqlDDLBaseListener {
-
-    final Logger logger = Logger.getLogger(DDLScriptListener.class.getName());
 
     final Map<String, TableDefinition> ddlObjectMap = new HashMap<>();
 
@@ -29,7 +53,7 @@ public class DDLScriptListener extends SqlDDLBaseListener {
 
     @Override
     public void visitErrorNode(ErrorNode node) {
-        logger.log(Level.SEVERE, node.toString());
+        log.error(node.toString());
     }
 
 
--- src/main/java/com/github/huluvu424242/sql/ddl/DDLScriptVisitor.java
+++ src/main/java/com/github/huluvu424242/sql/ddl/DDLScriptVisitor.java
@@ -1,26 +1,41 @@
 package com.github.huluvu424242.sql.ddl;
 
+/*-
+ * #%L
+ * java-compiler-template
+ * %%
+ * Copyright (C) 2023 PIUG
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
 
 import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLBaseVisitor;
 import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLParser;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-
+@Slf4j
 public class DDLScriptVisitor extends SqlDDLBaseVisitor<String> {
-    final Logger logger = Logger.getLogger(DDLScriptVisitor.class.getName());
-
-//    @Override
-//    public String visitParse(SqlDDLParser.ParseContext context) {
-//        visitChildren(context);
-//
-//        System.out.println("");
-//
-//        return null;
-//    }
 
     @Override
     public String visitCreate_table(SqlDDLParser.Create_tableContext context) {
@@ -50,7 +65,7 @@ public class DDLScriptVisitor extends SqlDDLBaseVisitor<String> {
         tableDefinitionBuilder.columns(columns);
         final TableDefinition tableDefinition = tableDefinitionBuilder.build();
 
-        logger.log(Level.INFO, () -> ">" + tableDefinition.toString() + "<");
+        log.info(">" + tableDefinition.toString() + "<");
 
         return visitChildren(context);
     }
