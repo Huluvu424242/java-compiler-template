4bb376da2d61bf18dccdab72543211f8f0a8099e
huluvu424242 <huluvu424242@gmail.com>
2023-04-23 10:12:04 +0200

    [feature/projectsetup] add visitor test
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/test/java/com/github/huluvu424242/sql/ddl/DDLScriptVisitorTest.java
+++ src/test/java/com/github/huluvu424242/sql/ddl/DDLScriptVisitorTest.java
@@ -1,12 +1,31 @@
 package com.github.huluvu424242.sql.ddl;
 
+import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLLexer;
+import com.github.huluvu424242.sql.ddl.antlr4.SqlDDLParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DDLScriptVisitorTest {
 
     @Test
     void visitCreate_table() {
+        final String ddlScriptContent =
+                """
+                        create table Mitarbeiter (
+                            id int not null primary key,
+                            vorname varchar(200),
+                            name varchar(2300)
+                        );
+                         """;
+        final CharStream inputStream = CharStreams.fromString(ddlScriptContent);
+        final SqlDDLLexer markupLexer = new SqlDDLLexer(inputStream);
+        final CommonTokenStream commonTokenStream = new CommonTokenStream(markupLexer);
+        final SqlDDLParser markupParser = new SqlDDLParser(commonTokenStream);
+        SqlDDLParser.ParseContext fileContext = markupParser.parse();
+        final DDLScriptVisitor visitor = new DDLScriptVisitor() {
+        };
+        visitor.visit(fileContext);
     }
 }
