a4f6c522f0c15e64db60e49c694a01b1a4c86e3f
huluvu424242 <huluvu424242@gmail.com>
2023-05-01 20:18:24 +0200

    [feature/qs] refactoring
    Signed-off-by: huluvu424242 <huluvu424242@gmail.com>

--- src/main/java/com/github/huluvu424242/plantuml/EntityModel.java
+++ src/main/java/com/github/huluvu424242/plantuml/EntityModel.java
@@ -26,12 +26,15 @@ package com.github.huluvu424242.plantuml;
  * #L%
  */
 
+import com.github.huluvu424242.sql.ddl.DDLColumnDefinition;
+import com.github.huluvu424242.sql.ddl.DDLTableDefinition;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Singular;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
@@ -63,6 +66,39 @@ public class EntityModel {
 
     @Singular
     protected List<Entity> entities = new ArrayList<>();
+
+    public static String of(final Map<String, DDLTableDefinition> schemaDefinition) {
+        final EntityModel.EntityModelBuilder diagramBuilder = EntityModel
+                .builder();
+
+        schemaDefinition
+                .entrySet()
+                .stream()
+                .map((Map.Entry<String, DDLTableDefinition> entry) -> {
+                    final DDLTableDefinition tableDefinition = entry.getValue();
+
+                    final Entity.EntityBuilder entityBuilder = Entity
+                            .builder()
+                            .name(tableDefinition.getTableName());
+                    // Spalten auswerten und ans entity anhängen
+                    tableDefinition
+                            .getColumns()
+                            .values()
+                            .stream()
+                            .map((DDLColumnDefinition colDef) -> ColumnSpec
+                                    .builder()
+                                    .colName(colDef.getColumnName())
+                                    .colType(colDef.getDataType())
+                                    .mandatory(Boolean.FALSE.equals(colDef.getNullable()))
+                                    .colNotes(Boolean.TRUE.equals(colDef.getPrimaryKey()) ? "<<PK>>" : "")
+                                    .build())
+                            .forEach(entityBuilder::column);
+                    return entityBuilder.build();
+                })
+                .forEach(diagramBuilder::entity);
+
+        return diagramBuilder.build().toString();
+    }
 
 
     protected States.UmlStartState getColumnDefinitionen(final States.EntityState state, final List<ColumnSpec> columns) {
